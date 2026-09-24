d70e4707c9c0bc84dcd855a365236a56628deeb8
Ole Magnus Fon Johnsen <me@omfj.no>
2026-09-24 20:21:48 +0200

    feat: more styles

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use tabled::{Table, Tabled, settings::Style};
 
 const TABLE_URL: &str = "https://www.eliteserien.no/tabell";
 
-#[derive(Tabled)]
+#[derive(Tabled, Default)]
 struct Team {
     /// The position in the league table
     #[tabled(rename = "Pos")]
@@ -42,19 +42,60 @@ struct Team {
     points: u32,
 }
 
-impl Default for Team {
-    fn default() -> Self {
-        Team {
-            position: 0,
-            name: String::new(),
-            played: 0,
-            wins: 0,
-            draws: 0,
-            losses: 0,
-            goals_for: 0,
-            goals_against: 0,
-            points: 0,
+struct Eliteserien {
+    url: String,
+}
+
+impl Eliteserien {
+    fn new(url: &str) -> Self {
+        Self {
+            url: url.to_string(),
         }
+    }
+
+    async fn get_table(&self) -> anyhow::Result<Vec<Team>> {
+        let response = reqwest::get(self.url.clone()).await?;
+        let body = response.text().await?;
+
+        let document = scraper::Html::parse_document(&body);
+        let selector = scraper::Selector::parse("table.table").unwrap();
+        let table = document.select(&selector).next().ok_or_else(|| {
+            anyhow::anyhow!("Failed to find the table with selector: {:?}", selector)
+        })?;
+        let rows_selector = scraper::Selector::parse("tr").unwrap();
+        let rows = table.select(&rows_selector);
+
+        let mut teams: Vec<Team> = Vec::new();
+
+        for (i, row) in rows.into_iter().enumerate() {
+            let cells_selector = scraper::Selector::parse("td").unwrap();
+            let cells = row.select(&cells_selector);
+
+            let mut team = Team {
+                position: i,
+                ..Team::default()
+            };
+
+            for (index, cell) in cells.enumerate() {
+                match index {
+                    1 => team.name = parse_team_name_from_cell(cell),
+                    2 => team.played = parse_number_from_cell(cell),
+                    3 => team.wins = parse_number_from_cell(cell),
+                    4 => team.draws = parse_number_from_cell(cell),
+                    5 => team.losses = parse_number_from_cell(cell),
+                    6 => team.goals_for = parse_number_from_cell(cell),
+                    7 => team.goals_against = parse_number_from_cell(cell),
+                    9 => team.points = parse_number_from_cell(cell),
+                    _ => continue,
+                }
+            }
+
+            if !team.name.is_empty() {
+                teams.push(team);
+            }
+        }
+
+        Ok(teams)
     }
 }
 
@@ -78,50 +119,18 @@ fn parse_team_name_from_cell(cell: ElementRef) -> String {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
-    let response = reqwest::get(TABLE_URL).await?;
-    let body = response.text().await?;
+    let args = std::env::args().collect::<Vec<String>>();
 
-    let document = scraper::Html::parse_document(&body);
-    let selector = scraper::Selector::parse("table.table").unwrap();
-    let table = document
-        .select(&selector)
-        .next()
-        .ok_or_else(|| anyhow::anyhow!("Failed to find the table with selector: {:?}", selector))?;
-    let rows_selector = scraper::Selector::parse("tr").unwrap();
-    let rows = table.select(&rows_selector);
-
-    let mut teams: Vec<Team> = Vec::new();
-
-    for (i, row) in rows.into_iter().enumerate() {
-        let cells_selector = scraper::Selector::parse("td").unwrap();
-        let cells = row.select(&cells_selector);
-
-        let mut team = Team {
-            position: i,
-            ..Team::default()
-        };
-
-        for (index, cell) in cells.enumerate() {
-            match index {
-                1 => team.name = parse_team_name_from_cell(cell),
-                2 => team.played = parse_number_from_cell(cell),
-                3 => team.wins = parse_number_from_cell(cell),
-                4 => team.draws = parse_number_from_cell(cell),
-                5 => team.losses = parse_number_from_cell(cell),
-                6 => team.goals_for = parse_number_from_cell(cell),
-                7 => team.goals_against = parse_number_from_cell(cell),
-                9 => team.points = parse_number_from_cell(cell),
-                _ => continue,
-            }
-        }
-
-        if !team.name.is_empty() {
-            teams.push(team);
-        }
-    }
+    let eliteserien = Eliteserien::new(TABLE_URL);
+    let teams = eliteserien.get_table().await?;
 
     let mut table = Table::new(teams);
-    table.with(Style::ascii_rounded());
+    match args.get(1).map(|s| s.as_str()) {
+        Some("ascii") => table.with(Style::ascii()),
+        Some("rounded") => table.with(Style::ascii_rounded()),
+        Some("modern") => table.with(Style::modern()),
+        _ => table.with(Style::ascii_rounded()),
+    };
     println!("{table}");
 
     Ok(())
